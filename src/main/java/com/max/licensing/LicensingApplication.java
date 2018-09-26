face32f2f5e729974ede421b253ee03a2ea0549d
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-09-26 12:47:35 +0300

    Fully fixed.

--- src/main/java/com/max/licensing/LicensingApplication.java
+++ src/main/java/com/max/licensing/LicensingApplication.java
@@ -3,7 +3,6 @@ package com.max.licensing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class LicensingApplication {
     public static void main(String[] args) {
