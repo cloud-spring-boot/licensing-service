182068de26ba117c51a5eb3943a4d3161c66f1f5
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-10-06 18:27:24 +0300

    Registration in Eureka added.

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+echo "********************************************************"
+echo "Waiting for the Eureka to start on port $EUREKASERVER_PORT"
+echo "********************************************************"
+while ! `nc -z eureka $EUREKASERVER_PORT `; do sleep 3; done
+echo ">>>>>>>>>>>> Eureka has started"
 
 echo "********************************************************"
 echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
@@ -16,6 +21,8 @@ echo "********************************************************"
 echo "Starting licensing-service "
 echo "********************************************************"
 java \
+-Djava.security.egd=file:/dev/./urandom \
+-Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI \
 -Dspring.cloud.config.uri=$CONFIGSERVER_URI \
 -Dspring.profiles.active=$PROFILE \
 -jar /usr/local/licensing-service/@project.build.finalName@.jar
--- src/main/java/com/max/licensing/LicensingApplication.java
+++ src/main/java/com/max/licensing/LicensingApplication.java
@@ -2,8 +2,10 @@ package com.max.licensing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LicensingApplication {
     public static void main(String[] args) {
         SpringApplication.run(LicensingApplication.class, args);
